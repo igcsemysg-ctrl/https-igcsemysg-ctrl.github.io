e52747944a3bdac94ead720b672ece18ba1c1d35
igcsemysg-ctrl <igcsemysg@gmail.com>
2026-07-21 12:57:16 +0800

    Update config.js

--- exam-intelligence/config.js
+++ exam-intelligence/config.js
@@ -1,5 +1,5 @@
 window.IGCSEMYSG_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  tutorFunctionName: "tutor"
+  supabaseUrl: "https://njaymwgiznqjholrjmxi.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qYXltd2dpem5xamhvbHJqbXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1OTkyMzEsImV4cCI6MjEwMDE3NTIzMX0.GvwtF9AmyfdX6hC6qAkoVBZA3zkXDlLwiBg9QW6Qtck",
+  tutorFunctionName: "IGCSEMYSG tutor"
 };
