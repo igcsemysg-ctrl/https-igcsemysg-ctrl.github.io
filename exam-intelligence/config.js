99f87dc1af22683ecf9c26b6a9689072602c2257
igcsemysg-ctrl <igcsemysg@gmail.com>
2026-07-21 13:49:37 +0800

    Delete exam-intelligence directory

--- exam-intelligence/config.js
+++
@@ -1,5 +0,0 @@
-window.IGCSEMYSG_CONFIG = {
-  supabaseUrl: "https://njaymwgiznqjholrjmxi.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qYXltd2dpem5xamhvbHJqbXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1OTkyMzEsImV4cCI6MjEwMDE3NTIzMX0.GvwtF9AmyfdX6hC6qAkoVBZA3zkXDlLwiBg9QW6Qtck",
-  tutorFunctionName: "IGCSEMYSG tutor"
-};
